59aa0759c7ff4ef888b3ef5cd5ca25677b465586
Lukin <Luk.bc@yandex.ru>
2023-06-01 16:28:04 +0400

    change4

--- src/test/java/ru/netology/rest/HomeworkTest.java
+++ src/test/java/ru/netology/rest/HomeworkTest.java
@@ -19,7 +19,7 @@ public class HomeworkTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("homework"))
         ;
 
     }
